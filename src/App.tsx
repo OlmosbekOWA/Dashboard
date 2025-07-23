a502133ee919dd8e15a9784724da49e06fff86b3
Olmosbek OWA <uzbmojor2810@gmail.com>
2025-07-23 19:40:48 +0500

    create my-dashboard

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Users from './pages/Users'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Users />
+      
+    </>
+  )
+}
+
+export default App
